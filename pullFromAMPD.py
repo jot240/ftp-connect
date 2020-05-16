fc6aa5665c6c605f126de45a0c8601524bdc7c69
jot240 <jot240@gmail.com>
2020-05-16 08:46:05 -0400

    added gitignore

--- pullFromAMPD.py
+++ pullFromAMPD.py
@@ -1,18 +1,21 @@
 import ftplib
-
+import os
 def main():
     file_name = 'allowances_csnox.zip' #downloading from EPA public FTP site
     file_path='DMDnLoad/allowances'
     host='newftp.epa.gov'
     ftp_as_zip(file_name, file_path, host)
 
-def ftp_as_zip(file_name,file_path, host):
+def ftp_as_zip(file_name,file_path, host, data_location = 'data'):
     print("connecting to: ", host)
     ftp =  ftplib.FTP(host)
     ftp.login()
     ftp.cwd(file_path)
-    print("opening file: "+file_name)
-    with open("DOWNLOAD"+file_name, "wb") as gFile:
+    print("data will be dumped to: " + data_location)
+    if not os.path.exists(data_location):
+        os.makedirs(data_location)
+    print("writing file: "+file_name)
+    with open(os.path.join(data_location,file_name), "wb") as gFile:
         ftp.retrbinary('RETR %s' % file_name, gFile.write)
         ftp.quit()
 
